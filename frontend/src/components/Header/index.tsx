d140cb399ff6dca09daad36f58de090b4ffa2f68
muutualswap <75495839+muutualswap@users.noreply.github.com>
2020-12-27 13:17:43 -0500

    Update index.tsx

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -171,6 +171,34 @@ const StyledNavLink = styled(NavLink)`
   min-width: 60px;
 `
 
+const StyledExternalLink = styled(ExternalLink).attrs({
+  activeClassName
+})<{ isActive?: boolean }>`
+  ${({ theme }) => theme.flexRowNoWrap}
+  align-items: left;
+  border-radius: 3rem;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text2};
+  font-size: 1rem;
+  width: fit-content;
+  margin: 0 12px;
+  font-weight: 500;
+  &.${activeClassName} {
+    border-radius: 12px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text1};
+  }
+  :hover,
+  :focus {
+    color: ${({ theme }) => darken(0.1, theme.text1)};
+  }
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+      display: none;
+`}
+`
+
 const StyledNavSwapLink = styled(StyledNavLink)`
   background: linear-gradient(180deg, #abf4fa 0%, rgba(171, 244, 250, 0) 100%);
   box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25);
