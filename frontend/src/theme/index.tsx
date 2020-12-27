d4119252d335959a5cd22a005b0dd91af07e64d6
muutualswap <75495839+muutualswap@users.noreply.github.com>
2020-12-27 08:01:15 -0500

    Update index.tsx

--- frontend/src/theme/index.tsx
+++ frontend/src/theme/index.tsx
@@ -1,3 +1,4 @@
+import { transparentize } from 'polished'
 import React, { useMemo } from 'react'
 import styled, {
   ThemeProvider as StyledComponentsThemeProvider,
@@ -5,10 +6,9 @@ import styled, {
   css,
   DefaultTheme
 } from 'styled-components'
-import { useIsDarkMode } from '../state/user/hooks'
+/*import { useIsDarkMode } from '../state/user/hooks'*/
 import { Text, TextProps } from 'rebass'
 import { Colors } from './styled'
-import bg from '../assets/svg/bg.jpeg'
 
 export * from './components'
 
@@ -21,7 +21,7 @@ const MEDIA_WIDTHS = {
 
 const mediaWidthTemplates: { [width in keyof typeof MEDIA_WIDTHS]: typeof css } = Object.keys(MEDIA_WIDTHS).reduce(
   (accumulator, size) => {
-    ;(accumulator as any)[size] = (a: any, b: any, c: any) => css`
+    ; (accumulator as any)[size] = (a: any, b: any, c: any) => css`
       @media (max-width: ${(MEDIA_WIDTHS as any)[size]}px) {
         ${css(a, b, c)}
       }
@@ -32,7 +32,7 @@ const mediaWidthTemplates: { [width in keyof typeof MEDIA_WIDTHS]: typeof css } 
 ) as any
 
 const white = '#FFFFFF'
-const black = '#000000'
+const black = '#313131'
 
 export function colors(darkMode: boolean): Colors {
   return {
@@ -41,33 +41,25 @@ export function colors(darkMode: boolean): Colors {
     black,
 
     // text
-    label: darkMode ? '#FFFFFF' : '#FFFFFF',
-    text1: darkMode ? '#FFFFFF' : '#FFFFFF',
-    text2: darkMode ? '#FFFFFF' : '#FFFFFF', // swap label
-    text3: darkMode ? '#FFFFFF' : '#FFFFFF',
+    text1: darkMode ? '#FFFFFF' : '#313131',
+    text2: darkMode ? '#C3C5CB' : '#565A69',
+    text3: darkMode ? '#6C7284' : '#888D9B',
     text4: darkMode ? '#565A69' : '#C3C5CB',
     text5: darkMode ? '#2C2F36' : '#EDEEF2',
 
     // backgrounds / greys
-    baseBg: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
-    header: darkMode ? '#EAFCFE' : '#EAFCFE',
-    swapBg: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
-    swapInput: darkMode ? '#527683' : '#527683',
-    swapSelect: darkMode ? '#ABF4FA' : '#ABF4FA',
-    globalBg: darkMode ? '#FFFFFF' : '#FFFFFF',
-    bg1: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
-    bg2: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
-    bg3: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
-    bg4: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
-    bg5: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
+    bg1: darkMode ? '#212429' : '#FFFFFF',
+    bg2: darkMode ? '#2C2F36' : '#F7F8FA',
+    bg3: darkMode ? '#40444F' : '#EDEEF2',
+    bg4: darkMode ? '#565A69' : '#CED0D9',
+    bg5: darkMode ? '#6C7284' : '#888D9B',
 
     //specialty colors
-    modalBG: darkMode ? 'rgba(0,0,0,0.4)' : 'rgba(0,0,0,0.4)',
-    modalCBG: darkMode ? 'rgba(55, 97, 111, 1)' : 'rgba(55, 97, 111, 1)',
-    advancedBG: darkMode ? 'rgba(55, 97, 111, 0.9)' : 'rgba(55, 97, 111, 0.9)',
+    modalBG: darkMode ? 'rgba(0,0,0,.425)' : 'rgba(0,0,0,0.3)',
+    advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#ABF4FA' : '#ABF4FA',
+    primary1: darkMode ? '#2172E5' : '#ff007a',
     primary2: darkMode ? '#3680E7' : '#FF8CC3',
     primary3: darkMode ? '#4D8FEA' : '#FF99C9',
     primary4: darkMode ? '#376bad70' : '#F6DDE8',
@@ -82,8 +74,8 @@ export function colors(darkMode: boolean): Colors {
     secondary3: darkMode ? '#17000b26' : '#FDEAF1',
 
     // other
-    red1: '#F19E9C',
-    red2: '#F19E9C',
+    red1: '#FF6871',
+    red2: '#F82D3A',
     green1: '#27AE60',
     yellow1: '#FFE270',
     yellow2: '#F3841E',
@@ -124,14 +116,14 @@ export function theme(darkMode: boolean): DefaultTheme {
 }
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const darkMode = useIsDarkMode()
+  const darkMode = true;/*useIsDarkMode()*/
 
   const themeObject = useMemo(() => theme(darkMode), [darkMode])
 
   return <StyledComponentsThemeProvider theme={themeObject}>{children}</StyledComponentsThemeProvider>
 }
 
-const TextWrapper = styled(Text)<{ color: keyof Colors }>`
+const TextWrapper = styled(Text) <{ color: keyof Colors }>`
   color: ${({ color, theme }) => (theme as any)[color]};
 `
 
@@ -185,29 +177,28 @@ export const TYPE = {
 
 export const FixedGlobalStyle = createGlobalStyle`
 html, input, textarea, button {
-  font-family: 'Century Gothic', sans-serif;
+  font-family: 'Inter', sans-serif;
   font-display: fallback;
 }
 @supports (font-variation-settings: normal) {
   html, input, textarea, button {
-    font-family: 'Century Gothic', sans-serif;
+    font-family: 'Inter var', sans-serif;
   }
 }
-
 html,
 body {
   margin: 0;
   padding: 0;
 }
-
+ a {
+   color: ${colors(false).blue1}; 
+ }
 * {
   box-sizing: border-box;
 }
-
 button {
   user-select: none;
 }
-
 html {
   font-size: 16px;
   font-variant: none;
@@ -215,33 +206,110 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   font-feature-settings: 'ss01' on, 'ss02' on, 'cv01' on, 'cv03' on;
+  
+  background: url(images/hexagons.png), -moz-linear-gradient(45deg, rgba(16, 114, 189, 1) 0%, rgba(16, 114, 189, 1) 23%, rgba(2, 41, 97, 1) 48%, rgba(0, 4, 38, 1) 73%, rgba(0, 0, 21, 1) 100%);
+			background: url(images/hexagons.png),-webkit-linear-gradient(45deg, rgba(16, 114, 189, 1) 0%, rgba(16, 114, 189, 1) 23%, rgba(2, 41, 97, 1) 48%, rgba(0, 4, 38, 1) 73%, rgba(0, 0, 21, 1) 100%);
+			background: url(images/hexagons.png),-o-linear-gradient(45deg, rgba(16, 114, 189, 1) 0%, rgba(16, 114, 189, 1) 23%, rgba(2, 41, 97, 1) 48%, rgba(0, 4, 38, 1) 73%, rgba(0, 0, 21, 1) 100%);
+			background: url(images/hexagons.png),-ms-linear-gradient(45deg, rgba(16, 114, 189, 1) 0%, rgba(16, 114, 189, 1) 23%, rgba(2, 41, 97, 1) 48%, rgba(0, 4, 38, 1) 73%, rgba(0, 0, 21, 1) 100%);
+			background: url(images/hexagons.png),linear-gradient(45deg, rgba(16, 114, 189, 1) 0%, rgba(16, 114, 189, 1) 23%, rgba(2, 41, 97, 1) 48%, rgba(0, 4, 38, 1) 73%, rgba(0, 0, 21, 1) 100%);
+			
+}
+div[data-reach-dialog-content] a
+{
+  color:#FFE600 ;
+}
+#center-logo
+{
+  margin-bottom:40px;
+  zoom:0.8;
+  position: relative;
+    z-index: 1;
+}
+#animate-zoom
+{
+  display:inline-block;
+  
+  animation: blink-animation 0.4s infinite;
+}
+@keyframes blink-animation {
+  form {
+    transform:scale(1)
+  }
+  to {
+    transform:scale(1.3)
+  }
+}
+.token-amount-input::placeholder
+{
+  color:rgba(255,255,255,0.5) !important;
+}
+#shadow_bottom
+		{
+			background:url(./images/shadow_bottom.png) repeat-x;
+			width:100%;
+			height:860px;
+			position:fixed;
+			bottom:0;
+			z-index:-1;
+    }
+    
+@media only screen and (max-width: 960px) {
+  #shadow_bottom
+		{
+      opacity:0.6;
+    }
+    #center-logo
+    {
+      display:none
+    }
+    #logo-symbol
+    {
+      display:none;
+    }
+    #logo-full
+    {
+      display:block !important;
+      margin:0 auto;
+      margin-top:20px;
+    }
+    #logo-wrapper
+    {
+      margin-right:0px;
+    }
+    #body-wrapper
+    {
+      box-shadow: 0 0 50px #01001de6;
+    }
+}
+#stake-liquidity-token div:first-child
+{
+  background: transparent !important;
+}
+div[data-reach-dialog-overlay] {
+  background:rgba(20,30,75,0.80) !important;
+}
+div[data-reach-dialog-content]{
+  box-shadow: 0 0 100px #01001de6 !important;
+}
+.bfqITV
+{
+  border-radius:0 !important;
 }
 `
 
 export const ThemedGlobalStyle = createGlobalStyle`
 html {
-  color: ${({ theme }) => theme.white};
+  color: ${({ theme }) => theme.text1};
+  background-color: ${({ theme }) => theme.bg2};
 }
-  :global {
-    .ant-select-selector {
-      background: ${({ theme }) => theme.swapInput};
-    }
-  }
-
 body {
-  color: ${({ theme }) => theme.label};
   min-height: 100vh;
-  background: url(${() => bg}) no-repeat top;
-  background-size: cover;
-  position: relative;
-  &:before{
-    width: 100%;
-    min-height: 100vh;
-    top: 0;
-    bottom: 0;
-    position: absolute;
-    content: '';
-    background-color: rgba(0,0,0, 0.4);
-  }
+  background-position: 0 -30vh;
+  background-repeat: no-repeat;
+  background-image: ${({ theme }) =>
+    `radial-gradient(50% 50% at 50% 50%, ${transparentize(0.9, theme.primary1)} 0%, ${transparentize(
+      1,
+      theme.bg1
+    )} 100%)`};
 }
 `
