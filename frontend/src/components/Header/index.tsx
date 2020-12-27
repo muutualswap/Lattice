71ac1a881501a48404abf10ef4ea1dffd43ec633
muutualswap <75495839+muutualswap@users.noreply.github.com>
2020-12-27 13:47:41 -0500

    Update index.tsx

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -171,7 +171,7 @@ const StyledNavLink = styled(NavLink)`
   min-width: 60px;
 `
 
-const StyledExternalLink = styled(ExternalLink)'
+const StyledExternalLink = styled(ExternalLink)
   ${({ theme }) => theme.flexRowNoWrap}
   align-items: left;
   border-radius: 3rem;
